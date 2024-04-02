335ebb025a3f1034943ff7617a7c69371353e77b
Samuel Rothermel <samuelrothermel57@gmail.com>
2024-04-02 16:23:35 -0400

    Add post request for adding professors. Update .gitignore to ignore HTTP request files for testing. Add scripts to package.json.

--- backend/routes/professors.js
+++ backend/routes/professors.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-router.get('/', async (req, res) => res.send(await prisma.professor.findMany()))
+router.use(express.json());
+
+router.get('/', async (req, res) => res.send(await prisma.professor.findMany()));
 
 router.get('/:firstName/:lastName', async (req, res) => {
     const { firstName, lastName } = req.params;
@@ -23,6 +25,23 @@ router.get('/:firstName/:lastName', async (req, res) => {
     } catch(err) {
         res.status(404).send(`Professor ${firstName} ${lastName} not found`);
     }
-})
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { firstName, lastName } = req.body;
+
+        const professor = await prisma.professor.create({
+            data: {
+                firstName: firstName,
+                lastName: lastName
+            }
+        });
+
+        res.send(professor);
+    } catch (err) {
+        res.status(500).send(`Failed to add new professor. Error: ${err}`)
+    }
+});
 
 module.exports = router;
