808337e010883d094133e2485c8ae382d3cf5757
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-03 16:42:41 -0400

    Add submit functionality for professor
    Added ability to edit and submit professor's first and last name

--- frontend/src/components/Professor.jsx
+++ frontend/src/components/Professor.jsx
@@ -1,4 +1,32 @@
-const Professor = ({ firstName, lastName }) => {
+import { useState } from 'react';
+
+const Professor = ({firstName, lastName, isAdmin}) => {
+    const [firstNameField, setFirstNameField] = useState(firstName);
+    const [lastNameField, setLastNameField] = useState(lastName);
+
+    const handlelastNameChange = (event) => {
+        setLastNameField(event.target.value);
+    }
+
+    const handlefirstNameChange = (event) => {
+        setFirstNameField(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        alert('submitted');
+        this.firstName = firstNameField;
+        this.lastName = lastNameField;
+      }
+
+    if (isAdmin) {
+        return (
+            <div>
+                <input type="text" name="firstName" value={firstNameField} onChange={handlefirstNameChange}/>
+                <input type="text" name="lastName" value={lastNameField} onChange={handlelastNameChange}/>
+                <button className="submit-button" onClick={handleSubmit}>Submit</button>
+            </div>
+        )
+    }
     return(
         <div>
             {firstName} {lastName}
