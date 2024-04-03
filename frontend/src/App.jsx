bdabbdafc0316b064c2f7269b05ef199d5ad2fda
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-03 14:06:06 -0400

    Add admin button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import Professor from './components/Professor';
+import { useState } from 'react';
 
 const App = () => {
+  const [isAdmin, setAdmin] = useState(false);
+
+  if (isAdmin) {
     return (
         <>
+            <button className="admin-button" onClick={() => setAdmin(false)}>Not Admin</button>
+            <h1>Admin</h1>
             <h1>Professors</h1>
             <Professor firstName="Scott" lastName="Brahaney"></Professor>
         </>
     );
+  } else{
+    return (
+      <>
+          <button className="admin-button" onClick={() => setAdmin(true)}>Admin</button>
+          <h1>Not Admin</h1>
+          <h1>Professors</h1>
+          <Professor firstName="Scott" lastName="Brahaney"></Professor>
+      </>
+    );
+  }
 }
-
 export default App;
