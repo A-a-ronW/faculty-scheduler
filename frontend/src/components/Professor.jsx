cc7a4379c0842d918d9eeba29b9707a07dcb15a3
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-10 20:46:44 -0400

    Add Events in Professor component
    Added functionality for the Professor component to parse the Events table to display every event a professor is tied to.

--- frontend/src/components/Professor.jsx
+++ frontend/src/components/Professor.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import professorsService from "../services/professors.js";
+import Event from './Event';
+import eventsService from '../services/events.js';
 
 const Professor = ({id, professorsList, setProfessorsList, firstName, lastName, isAdmin}) => {
     // TODO: Consider omitting firstName and lastName props in favor of using the professorsList and id to get those values.
 
     const [firstNameField, setFirstNameField] = useState(firstName);
     const [lastNameField, setLastNameField] = useState(lastName);
+    const [eventsList, setEventsList] = useState([]);
+    const [newEventTitleField, setNewEventTitleField] = useState("");
+
+    useEffect(() => {
+        eventsService.getEvents()
+            .then(response => {
+                console.log(response);
+                setEventsList(response);
+            })
+            .catch(error => {
+                console.error(`Error fetching events: ${error}`);
+            });
+    }, []);
 
     const handleFirstNameChange = (event) => {
         setFirstNameField(event.target.value);
@@ -15,9 +30,13 @@ const Professor = ({id, professorsList, setProfessorsList, firstName, lastName, 
         setLastNameField(event.target.value);
     };
 
+    const handleNewEventTitleChange = (event) => {
+        setNewEventTitleField(event.target.value);
+    }
+
 
     const handleUpdateProfessor = () => {
-        // TODO: Add change confirmation message.
+        alert(`Updated ${firstName} ${lastName}`);
 
         const updatedProfessor = {
             firstName: firstNameField,
@@ -39,20 +58,65 @@ const Professor = ({id, professorsList, setProfessorsList, firstName, lastName, 
         }
     };
 
+    const handleCreateEvent = () => {
+        const createEventBody = {
+            title : newEventTitleField,
+            professorId: id
+        }
+
+        eventsService.createEvent(createEventBody).then(response => {
+            setEventsList(eventsList.concat(response));
+            setNewEventTitleField("");
+        });
+    }
+
     if (isAdmin) {
         return(
             <div>
                 <input type="text" name="firstName" value={firstNameField} onChange={handleFirstNameChange}/>
                 <input type="text" name="lastName" value={lastNameField} onChange={handleLastNameChange}/>
-                <button className="submit-button" onClick={() => handleDeleteProfessor()}>Delete</button>
-                <button className="submit-button" onClick={() => handleUpdateProfessor()}>Update</button>
+                <button className="update-button" onClick={() => handleUpdateProfessor()}>Update</button>
+                <button className="delete-button" onClick={() => handleDeleteProfessor()}>Delete</button>
+
+                <ul>
+                    {eventsList.map((event) => 
+                    event.professorId == id ? (
+                        <Event
+                            key={event.id}
+                            id={event.id}
+                            eventsList={eventsList}
+                            setEventsList={setEventsList}
+                            title={event.title}
+                            professorId={event.professorId}
+                            isAdmin={isAdmin}
+                        ></Event>
+                    ): null)}
+                    <div>
+                        <input type="text" name="newEventTitleField" value={newEventTitleField} onChange={handleNewEventTitleChange}/>
+                        <button className="submit-button" onClick={() => handleCreateEvent()}>Add</button>
+                    </div>
+                </ul>
             </div>
         );
     }
 
     return(
-        <div>
+        <div className='prof-div'>
             {firstName} {lastName}
+            <ul>
+                {eventsList.map((event) => 
+                    event.professorId == id ? (
+                        <Event
+                            key={event.id}
+                            id={event.id}
+                            eventsList={eventsList}
+                            setEventsList={setEventsList}
+                            title={event.title}
+                            professorId={event.professorId}
+                            isAdmin={isAdmin}
+                        ></Event>
+                    ): null)}
+            </ul>
         </div>
     );
 };
