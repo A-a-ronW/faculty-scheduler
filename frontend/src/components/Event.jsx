fcb64d71f3a64eacb12cfb1aab448007a9811369
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-10 20:45:06 -0400

    Create Event component
    Created Event component to function at a basic level. Only displays name and is tied to professor which it is created under.

---
+++ frontend/src/components/Event.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import eventsService from '../services/events';
+
+const Event = ({id, eventsList, setEventsList, title, isAdmin}) => {
+
+    const [titleField, setTitleField] = useState(title);
+
+    const handleUpdateEvent = () => {
+        alert(`Updated ${title}`);
+
+        const updatedEvent = {
+            title: titleField,
+        };
+
+        eventsService.updateEvent(id, updatedEvent).then((response) => {
+            setEventsList(eventsList.map(event => event.id === id ? response : event))
+        })
+    }
+
+    const handleDeleteEvent = () => {
+        const confirmation = confirm(`Do you really want to delete ${title}? Press OK to confirm.`);
+
+        if (confirmation) {
+            eventsService.deleteEvent(id).then(() => {
+                setEventsList(eventsList.filter(event => event.id !== id));
+            });
+        }
+    };
+
+    const handleTitleChange = (event) => {
+        setTitleField(event.target.value);
+    }
+
+    if (isAdmin) {
+        return (
+            <div>
+                <input type="text" name="title" value={titleField} onChange={handleTitleChange}/>
+                <button className="update-button" onClick={() => handleUpdateEvent()}>Update</button>
+                <button className="delete-button" onClick={() => handleDeleteEvent()}>Delete</button>
+            </div>
+        );
+    }
+    return (
+        <div>
+            <li>{title}</li>
+        </div>
+    );
+}
+
+export default Event;
