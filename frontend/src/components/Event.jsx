ab57934c5d1747885643daba0014c5dfc1f33a1d
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-13 13:37:49 -0400

    Add time fields for events
    Added time fields for events in admin view. Does not yet update database.

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import eventsService from '../services/events';
 
-const Event = ({id, eventsList, setEventsList, title, isAdmin}) => {
+const Event = ({id, eventsList, setEventsList, title, startTime, endTime, isAdmin}) => {
 
     const [titleField, setTitleField] = useState(title);
+    const [startTimeField, setStartTimeField] = useState('');
+    const [endTimeField, setEndTimeField] = useState('');
 
     const handleUpdateEvent = () => {
         alert(`Updated ${title}`);
 
         const updatedEvent = {
             title: titleField,
+            startTime: startTimeField,
+            endTime: endTimeField
         };
 
         eventsService.updateEvent(id, updatedEvent).then((response) => {
@@ -31,10 +35,20 @@ const Event = ({id, eventsList, setEventsList, title, isAdmin}) => {
         setTitleField(event.target.value);
     }
 
+    const handleStartTimeChange = (event) => {
+        setStartTimeField(event.target.value);
+    }
+
+    const handleEndTimeChange = (event) => {
+        setEndTimeField(event.target.value);
+    }
+
     if (isAdmin) {
         return (
             <div>
                 <input type="text" name="title" value={titleField} onChange={handleTitleChange}/>
+                <input type="text" name="startTime" value={startTimeField} onChange={handleStartTimeChange}></input>
+                <input type="text" name="endTime" value={endTimeField} onChange={handleEndTimeChange}></input>
                 <button className="update-button" onClick={() => handleUpdateEvent()}>Update</button>
                 <button className="delete-button" onClick={() => handleDeleteEvent()}>Delete</button>
             </div>
