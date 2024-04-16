a3325bcf71ba50a2035e1ea350cd7ef2d0a11dc9
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-16 16:55:00 -0400

    update ProfessorUser.jsx
    Added switch statement to only pass in list of events for current day into checkAvailability function

--- frontend/src/components/ProfessorUser.jsx
+++ frontend/src/components/ProfessorUser.jsx
@@ -3,13 +3,23 @@ import EventsGrouped from "./EventsGrouped";
 import {groupEventsByDay} from "../utils/eventUtil";
 import {checkAvailability} from "./../utils/timeUtils";
 
+const passCurrentDay = (groupedEvents) => {
+    switch(new Date().getDay()) {
+        case 0: return groupedEvents.SUNDAY;
+        case 1: return groupedEvents.MONDAY;
+        case 2: return groupedEvents.TUESDAY;
+        case 3: return groupedEvents.WEDNESDAY;
+        case 4: return groupedEvents.THURSDAY;
+        case 5: return groupedEvents.FRIDAY;
+        case 6: return groupedEvents.SATURDAY;
+    }
+}
 const ProfessorUser = ({ professor }) => {
     const groupedEvents = groupEventsByDay(professor.events.concat());
-
     return(
         <>
             <div className='prof-div'>
-                <h2>{professor.firstName} {professor.lastName} <Availability isAvailable={checkAvailability(professor.events)}/></h2>
+                <h2>{professor.firstName} {professor.lastName} <Availability isAvailable={checkAvailability(passCurrentDay(groupedEvents))}/></h2>
                 <EventsGrouped day={"MONDAY"} groupedEvents={groupedEvents.MONDAY} />
                 <EventsGrouped day={"TUESDAY"} groupedEvents={groupedEvents.TUESDAY} />
                 <EventsGrouped day={"WEDNESDAY"} groupedEvents={groupedEvents.WEDNESDAY} />
