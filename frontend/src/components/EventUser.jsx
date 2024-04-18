3c281e6e09b0ea839164554eca4bbc7d0f63e6cd
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-18 12:12:03 -0400

    Add display for time in User view
    Added display for time for standard users

--- frontend/src/components/EventUser.jsx
+++ frontend/src/components/EventUser.jsx
@@ -2,12 +2,9 @@ import formatTime from "./../utils/timeUtils";
 import formatDays from "./../utils/dayUtils";
 
 const EventUser = ({ event }) => {
-    const startDateObj = new Date(event.startTime);
-    const endDateObj = new Date(event.endTime);
-
     return(
         <div>
-            <div>{event.title} {formatTime(startDateObj)} to {formatTime(endDateObj)} {formatDays(event.days)}</div>
+            <div>{event.title} {event.startTime} to {event.endTime} {formatDays(event.days)}</div>
         </div>
     );
 };
