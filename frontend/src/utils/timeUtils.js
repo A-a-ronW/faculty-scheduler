e65460a1f792343721573e4832cec77fcb427bf2
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-04-20 18:49:35 -0400

    Update timeUtils.js
    Change checkAvailability to assume professors are unavailable outside 9am-5pm unless they have classes before/after those times, respectively

--- frontend/src/utils/timeUtils.js
+++ frontend/src/utils/timeUtils.js
@@ -9,16 +9,43 @@ const formatTime = (date) => {
     return hours + ':' + minutes + ' ' + ampm;
 };
 
+const earliestStartTime = (events) => {
+    let earliest = new Date();
+    //default is 9am
+    earliest.setHours(9);
+    earliest.setMinutes(0);
+    for (let i = 0; i < events.length; i++) {
+        let start = new Date(events[i].startTime)
+        earliest = ((earliest.getHours() >  start.getHours()) || ((earliest.getHours() === start.getHours()) && (earliest.getMinutes() > start.getMinutes()))) ? new Date(events[i].startTime) : earliest;
+    }
+    return earliest;
+};
+
+const latestEndTime = (events) => {
+    let latest = new Date();
+    //default is 5pm
+    latest.setHours(17);
+    latest.setMinutes(0);
+    for (let i = 0; i < events.length; i++) {
+        let end = new Date(events[i].startTime)
+        latest = ((latest.getHours() < end.getHours()) || ((latest.getHours() === end.getHours()) && (latest.getMinutes() < end.getMinutes()))) ? new Date(events[i].startTime) : latest;
+    }
+    return latest;
+};
+
 const checkAvailability = (events) => {
     //Is getting called twice per prfessor for some reason
     //Should be non-issue but noting in case of future errors
     let current_time = new Date();
+
+    let earliest = earliestStartTime(events);
+    let latest = latestEndTime(events);
+    if (current_time < earliest || current_time > latest) { return false;}
+
     for (let i = 0; i < events.length; i++) {
-        console.log(events[i].professorId);
         let start = new Date(events[i].startTime);
         let end = new Date(events[i].endTime);
 
-        console.log(start, current_time, end);
         let availability = (start <= current_time && end >= current_time) ? false : true;
         if (availability == false) { return false;}
     }
